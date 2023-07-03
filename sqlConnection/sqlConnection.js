97541b36b73bcc20e8a11ce619f7b4318c397b35
Zephyr <Zephyrtmh@gmail.com>
2023-07-03 23:27:47 +0800

    made changes to package.json and sql connection to set up database upon startup

--- sqlConnection/sqlConnection.js
+++ sqlConnection/sqlConnection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 //setup mysql connection
-mySqlConnection = mysql
+const mySqlConnection = mysql
     .createPool({
         host: "localhost",
         user: "root",
@@ -9,5 +9,40 @@ mySqlConnection = mysql
         database: "tmsdb",
     })
     .promise();
+
+const setUpDatabase = async () => {
+    //query to create DB
+    const createDB = "CREATE DATABASE IF NOT EXISTS tmsdb;";
+
+    //query to create accounts table
+    const createAccounts = 
+    `CREATE TABLE accounts (
+        username varchar(50) PRIMARY KEY,
+        password varchar(100),
+        email varchar(100),
+        active varchar (20)
+    );`;
+
+    //query to create accounts_usergroups table
+    const createAccountsUserGroups = 
+    `CREATE TABLE accounts_usergroups (
+        username varchar(50),
+        userGroupName varchar(50),
+        PRIMARY KEY (username, userGroupName)
+    );`;
+
+    //query to create accounts_usergroups table
+    const createUserGroups = 
+    `CREATE TABLE user_group (
+        userGroupName varchar(50) PRIMARY KEY
+    );`;
+
+    await mySqlConnection.query(createDB);
+    await mySqlConnection.query(createAccounts);
+    await mySqlConnection.query(createAccountsUserGroups);
+    await mySqlConnection.query(createUserGroups);
+    
+    return;
+}
 
 module.exports = mySqlConnection;
